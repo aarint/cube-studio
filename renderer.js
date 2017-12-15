27b7af797ca5dd68f2a2c9d5271eefa189348570
aarint <aardoit@gmail.com>
2017-12-15 15:58:02 +0800

    Combine the react app with electron.

--- renderer.js
+++
@@ -1,4 +0,0 @@
-import {clipboard, remote} from 'electron';
-
-let currentWindow = remote.getCurrentWindow();
-console.log(currentWindow); 
--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var os = window.require('os');
+console.log("got:", os.cpus());
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          {os.cpus().length}
+        </div>
       </div>
     );
   }
